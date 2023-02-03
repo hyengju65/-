ec24d539697811b250087594f50a1047b7efee55
hyengju65 <hyengju65@naver.com>
2023-02-03 17:27:45 +0900

    study 배열과 for문

--- 0203/0203/array.cpp
+++ 0203/0203/array.cpp
@@ -1,26 +1,51 @@
-#include<iostream>
+#include <iostream>
 int main() {
-	std::string friend1 = "È«±æµ¿";
-	std::string friend2 = "¼ºÃáÇâ";
+	double nums[3] = { 1.5, 2.5, 3.5 };
 
-	std::string friends[2];
+	std::string fruit[2][3] = {
+		{ "apple", "banana", "orange"},
+		{ "apple2", "banana2", "orange2" }
+	};
 
-	friends[0] = "È«±æµ¿";
-	friends[1] = "¼ºÃáÇâ";
-	friends[1] = "ÄÚµù¿Â";
+	// fruit[1] = { "apple2", "banana2", "orange2" }
+
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 3; j++) {
+			std::cout << fruit[i][j] << std::endl;
+		}
+	}
+
+	for (auto& f : fruit) {
+		for (std::string f2 : f) {
+			std::cout << f2 << std::endl;
+		}
+	}
+
+	std::cout << fruit << std::endl;
+
+	std::string friends[] = { "È«±æµ¿", "¼ºÃáÇâ", "ÄÚµù¿Â" };
+	/*std::cout << friends[0] << std::endl;
+	std::cout << friends[1] << std::endl;
+	std::cout << friends[2] << std::endl;*/
+
+	for (int i = 0; i < 3; i++) {
+		std::cout << friends[i] << std::endl;
+	}
+
+	// sizeof() : sizeof ¾È¿¡ º¯¼ö or typeÀ» ÀÎÀÚ·Î ³Ñ°ÜÁÜ. Å©±â¸¦ byte´ÜÀ§·Î ±¸ÇÔ.
+	// sizeof(friends)/sizeof(friends[0])
+	for (int i = 0; i < sizeof(friends) / sizeof(std::string); i++) {
+		std::cout << friends[i] << std::endl;
+	}
+
+	for (std::string f : friends) {
+		// f = "È«±æµ¿"
+		// f = "¼ºÃáÇâ" ·Î ¹Ù²ñ
+		// f = "ÄÚµù¿Â" ·Î ¹Ù²ñ
+		std::cout << f << std::endl;
+	}
 
 
-	std::cout << friends[1];
-
-	int i;
-	i = 0;
-	int i = 0;
-
-	std::string friends[2] = { "È«±æµ¿", "¼ºÃáÇâ" };
-
-	int num[3] = { 1,2,3 };
-
-	std::string fruit[2][2] = { {"apple","grape"},{"orange","banana"}};
-
-
+	// friends = { "È«±æµ¿", "¼ºÃáÇâ" }
+	return 0;
 }
