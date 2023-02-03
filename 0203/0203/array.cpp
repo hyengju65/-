979afb77c9f86c509e00bbe903e17e846754450d
hyengju65 <hyengju65@naver.com>
2023-02-03 16:16:36 +0900

    study:배열선언

---
+++ 0203/0203/array.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+int main() {
+	std::string friend1 = "全辨悼";
+	std::string friend2 = "己冕氢";
+
+	std::string friends[2];
+
+	friends[0] = "全辨悼";
+	friends[1] = "己冕氢";
+	friends[1] = "内爹柯";
+
+
+	std::cout << friends[1];
+
+	int i;
+	i = 0;
+	int i = 0;
+
+	std::string friends[2] = { "全辨悼", "己冕氢" };
+
+	int num[3] = { 1,2,3 };
+
+	std::string fruit[2][2] = { {"apple","grape"},{"orange","banana"}};
+
+
+}
